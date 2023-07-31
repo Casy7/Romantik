e0540b8a281381be4a945be0341baf1b3e724973
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2023-08-01 01:15:34 +0300

    added fonts and other libraries

--- RomantikApp/views.py
+++ RomantikApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
 # Create your views here.
+
+
+def base_context(request, **args):
+    context = {}
+    django_user = request.user
+
+    context['title'] = 'none'
+    context['header'] = 'none'
+    context['error'] = 0
+    context['is_superuser'] = False
+
+    if args != None:
+        for arg in args:
+            context[arg] = args[arg]
+
+    return context
+
+
+class HomePage(View):
+    def get(self, request):
+        context = base_context(request, title='Home')
+        return render(request, "home.html", context)
--- romantik/settings.py
+++ romantik/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-(1k2#-4ki&(z04ur=jg+i^_1s7q-516a_li1+(e22*mv78!_!c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['romantik.pythonanywhere.com']
+ALLOWED_HOSTS = ['romantik.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'romantik.urls'
 
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'RomantikApp', 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +131,4 @@ MEDIA_ROOT = '/home/romantik/romantik/media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/romantik/romantik/static'
 STATIC_URL = '/static/'
+
