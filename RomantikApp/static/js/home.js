1b040d0780e2e9ae8b29924ac94c220e96a68904
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2023-08-13 13:55:34 +0300

    Minor bugfix

--- RomantikApp/static/js/home.js
+++ RomantikApp/static/js/home.js
@@ -3,8 +3,8 @@ const map = new mapboxgl.Map({
 container: 'map', // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [30.76, 46.44903], // starting position [lng, lat]
-zoom: 15 // starting zoom
+center: [30.755, 46.45], // starting position [lng, lat]
+zoom: 13 // starting zoom
 });
 
 const marker1 = new mapboxgl.Marker().setLngLat([30.74983, 46.44903]).addTo(map);
