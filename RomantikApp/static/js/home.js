ac5f329984221634e0ef1e851a9645e09b85a316
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2023-08-13 14:15:55 +0300

    mobile mapbox map centered to the club

--- RomantikApp/static/js/home.js
+++ RomantikApp/static/js/home.js
@@ -3,9 +3,15 @@ const map = new mapboxgl.Map({
 container: 'map', // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [30.755, 46.45], // starting position [lng, lat]
+center: [30.77, 46.45], // starting position [lng, lat]
 zoom: 13 // starting zoom
 });
+
+if (window.mobileCheck()) {
+	map.flyTo({
+		center: [30.751867631916582,  46.44910605734739]
+		});
+}
 
 const marker1 = new mapboxgl.Marker().setLngLat([30.74983, 46.44903]).addTo(map);
  
