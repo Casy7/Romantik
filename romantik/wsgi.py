70320355aae05965de16a4362a7840896a6024d1
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2024-01-04 15:41:04 +0200

    Changes sys PATH dir

--- romantik/wsgi.py
+++ romantik/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+paths = ['/home/romantik/romantik', '/home/casy7/romantik']
+for path in paths:
+	if path not in sys.path:
+		sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'romantik.settings')
-
 application = get_wsgi_application()
