fbed772061e2bcd16cdb191caf272a166e498422
Casy <yarik.casy.od@gmail.com>
2024-10-19 09:02:04 +0300

    Added models to admin panel

--- KaptyorkaApp/admin.py
+++ KaptyorkaApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cathegory', 'price', 'amount')
+    search_fields = ('name', 'cathegory', 'price', 'amount')
+
+admin.site.register(Equipment, EquipmentAdmin)
+
+class CathegoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent_cathegory')
+    search_fields = ('name', 'parent_cathegory')
+
+admin.site.register(Cathegory, CathegoryAdmin)
